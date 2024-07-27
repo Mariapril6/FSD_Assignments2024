e60bdea4779fc57c257bf4d254ffc2ebc6ba1be9
Mariapril6 <maria.rabbani1985@gmail.com>
2024-07-28 02:49:45 +0500

    The useEffect Assignment
    Data fetched from API given, using useEffect and displayed on the web page.

--- UseEffectAssignment.js
+++ UseEffectAssignment.js
@@ -1,24 +1,35 @@
 import { useEffect, useState } from "react";
 const UseEffectAssignment = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://datausa.io/api/data?drilldowns=Nation&measures=Population"
+        );
+        const result = await response.json();
+        console.log(result);
+        setData(result.data);
+
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="container-useEffect">
-      {useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch(
-              "https://datausa.io/api/data?drilldowns=Nation&measures=Population"
-            );
-            const result = await response.json();
-            console.log(result);
-            setData(result);
-            console.log({ data });
-          } catch (error) {
-            console.log(error);
-          }
-        };
-        fetchData();
-      }, [])}
+      <h1>The USA Population over the last 10 years..</h1>
+      {data &&
+        data.map((item) => {
+          return (
+            <p key={item.Year}>
+              The population of United States in the Year {item.Year} was{" "}
+              {item.Population}.
+            </p>
+          );
+        })}
     </div>
   );
 };
